3d2cbcc970f38bf3687acadc7edebe45cdd402de
Doston <doston@Dostons-MacBook-Pro.local>
2019-12-13 18:58:30 +0500

    Asil kot

--- api/check.php
+++ api/check.php
@@ -6,11 +6,18 @@
         "date_error"=>"empty",
         "no_of_people"=>"empty"
     );
+    $messages1 = array(
+        "name_error"=>"no error",
+        "email_error"=>"name",
+        "phone_error"=>"no error",
+        "date_error"=>"empty",
+        "no_of_people"=>"empty"
+    );
     
     echo json_encode($message);
-    print_r($message);
     $messages = array();
-    for($i=0;$i<count($message); $i++){
-        array_push($messages,$message[$i]);
-    }
+    
+        array_push($messages,$message);
+        array_push($messages,$messages1);
+
     echo json_encode($messages);
