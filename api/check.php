9300e6db49d2848528002c7e9bb1cb1c67acf318
Doston <doston@Dostons-MacBook-Pro.local>
2019-12-13 17:31:33 +0500

    Asil kot

---
+++ api/check.php
@@ -0,0 +1,10 @@
+<?php
+    $message = array(
+        "name_error"=>"no error",
+        "email_error"=>"name",
+        "phone_error"=>"no error",
+        "date_error"=>"empty",
+        "no_of_people"=>"empty"
+    );
+
+    echo json_encode($message);
